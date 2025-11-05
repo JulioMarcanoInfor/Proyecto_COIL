3071fcef322291f8b678d28e9dc722509c8c3c07
julio marcano <juliocesarmarcanochacare@gmail.com>
2025-11-04 21:26:35 -0400

    Creacion de primer panel dinamico

---
+++ proyecto_COIL/src/com/panaderiamatemagica/core/Main.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package com.panaderiamatemagica.core;
+import com.panaderiamatemagica.core.controlador.RouterControlador;
+import com.panaderiamatemagica.core.visual.PantallaPrincipalVista;
+/**
+ *
+ * @author User
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //este codigo crea una instancia de RouterCntrolador y ese codigo extraño es necesario para que la interfaz grafica funcione
+        java.awt.EventQueue.invokeLater(() -> {
+            new RouterControlador(); 
+        });
+    }
+    
+}
